e5e192d678f55d7388363f5e5189beae5b199c8b
Daniel Gaida <daniel.gaida@th-koeln.de>
2025-12-13 22:18:32 +0100

    fixed init

--- redis_robot_comm/__init__.py
+++ redis_robot_comm/__init__.py
@@ -1,7 +1,8 @@
 from .redis_client import RedisMessageBroker
 from .redis_image_streamer import RedisImageStreamer
 from .redis_label_manager import RedisLabelManager
+from .redis_text_overlay import RedisTextOverlayManager
 
-__all__ = ["RedisMessageBroker", "RedisImageStreamer", "RedisLabelManager"]
+__all__ = ["RedisMessageBroker", "RedisImageStreamer", "RedisLabelManager", "RedisTextOverlayManager"]
 
 __version__ = "0.1.0"
